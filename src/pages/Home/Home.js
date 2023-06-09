40edf1a5675bdf2d9180e79f7a7e9c3293293241
nadiras <nadiraomar98@gmail.com>
2023-06-09 15:32:36 -0500

    home page styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,16 @@
+import './Home.css'
+import Home from '../../assets/home-page-img.png'
 
 export default function NavBar(){
     return(
-        <h1>This is the Home Page</h1>
+        <div className="container">
+            <div className="text">
+                <h2>Plan A Delicious Trip</h2>
+                <p>Explore a number of different restaurants on your upcoming trip and plan an experience you won't forget for every meal!</p>
+            </div>
+            <div className="image">
+            <img src={Home} title="home-img" alt="pizza-in-venice" />
+            </div>
+        </div>
     )
 }
